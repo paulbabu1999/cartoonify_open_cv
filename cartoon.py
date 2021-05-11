84717e2fb5a63d1f1b5f4af19af5bc1f4e67c05f
paulbabu1999 <33522991+paulbabu1999@users.noreply.github.com>
2021-05-11 14:10:21 +0530

    Create cartoon.py

---
+++ cartoon.py
@@ -0,0 +1,51 @@
+import cv2 #for image processing
+import numpy as np #to store image
+import imageio #to read image stored at particular path
+import sys
+import matplotlib.pyplot as plt
+import os
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
+#read the image
+ImagePath="/content/IMG_5937.jpg" #give path of image
+originalmage = cv2.imread(ImagePath)
+originalmage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2RGB)
+#print(image)  # image is stored in form of numbers
+
+# confirm that image is chosen
+if originalmage is None:
+    print("Can not find any image. Choose appropriate file")
+    sys.exit()
+
+ReSized1 = cv2.resize(originalmage, (960, 540))
+plt.imshow(ReSized1, cmap='gray')
+#converting an image to grayscale
+grayScaleImage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2GRAY)
+ReSized2 = cv2.resize(grayScaleImage, (960, 540))
+plt.imshow(ReSized2, cmap='gray')
+#applying median blur to smoothen an image
+smoothGrayScale = cv2.medianBlur(grayScaleImage, 5)
+ReSized3 = cv2.resize(smoothGrayScale, (960, 540))
+plt.imshow(ReSized3, cmap='gray')
+
+#retrieving the edges for cartoon effect
+#by using thresholding technique
+getEdge = cv2.adaptiveThreshold(smoothGrayScale, 255, 
+  cv2.ADAPTIVE_THRESH_MEAN_C, 
+  cv2.THRESH_BINARY, 9, 9)
+
+ReSized4 = cv2.resize(getEdge, (960, 540))
+plt.imshow(ReSized4, cmap='gray')
+
+#applying bilateral filter to remove noise 
+#and keep edge sharp as required
+colorImage = cv2.bilateralFilter(originalmage, 9, 300, 300)
+ReSized5 = cv2.resize(colorImage, (960, 540))
+plt.imshow(ReSized5, cmap='gray')
+#masking edged image with our "BEAUTIFY" image
+cartoonImage = cv2.bitwise_and(colorImage, colorImage, mask=getEdge)
+
+ReSized6 = cv2.resize(cartoonImage, (960, 540))
+plt.imshow(ReSized6, cmap='gray')
